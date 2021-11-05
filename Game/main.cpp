3a4d50d8bbe99b52623ec0fff48adf264fde2f73
Jumanazarov-Shukrullo <shukrullo.jumanazarov@phystech.edu>
2021-11-05 09:11:09 +0300

    deleted

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,30 +1,15 @@
 #include <QApplication>
+#include "Player.h"
 #include <QGraphicsScene>
-#include "MyRect.h"
 #include <QGraphicsView>
+#include <QTimer>
+#include <Game.h>
 
+Game * game;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     QApplication a(argc, argv);
-
-    QGraphicsScene *scene = new QGraphicsScene();
-
-    MyRect *rect = new MyRect();
-    rect->setRect(0,0,100,100);
-    rect->setFlag(QGraphicsItem::ItemIsFocusable);
-    rect->setFocus();
-    scene->addItem(rect);
-
-    QGraphicsView *v = new QGraphicsView(scene);
-    v->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    v->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-
-    v->show();
-    v->setFixedSize(800, 600);
-    scene->setSceneRect(0,0,800,600);
-    rect->setPos(v->width() / 2, v->height() - rect->rect().height());
-
+    game = new Game();
+    game->show();
     return a.exec();
 }
