fc4806922135f5dbd463351eec331f3a2c3b2336
Jumanazarov-Shukrullo <shukrullo.jumanazarov@phystech.edu>
2021-11-16 05:46:24 +0300

    Added new files

---
+++ PlaneGame/src/Source/Score.cpp
@@ -0,0 +1,18 @@
+#include "../Headers/Score.h"
+#include <QFont>
+
+Score::Score(QGraphicsItem *parent) : QGraphicsTextItem(parent) {
+    score_ = 0;
+    setPlainText("Score: " + QString::number(score_));
+    setDefaultTextColor(Qt::yellow);
+    setFont(QFont("times", 16));
+}
+
+void Score::IncreaseScore() {
+    score_++;
+    setPlainText("Score: " + QString::number(score_));
+}
+
+int Score::GetScore() {
+    return score_;
+}
---
+++ PlaneGame/src/Source/main.cpp
@@ -0,0 +1,12 @@
+#include "../Headers/Game.h"
+#include <QApplication>
+#include <QTimer>
+
+Game *game;
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    game = new Game();
+    game->show();
+    return a.exec();
+}
